d4e82f223f18e6b7309660e696fcb2db4bdb243a
Ganesh <justganesh12321@gmail.com>
2021-08-02 00:23:53 +0530

    docker added

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@el=onrii5ulx^8vp31$i3lep(+!3b)^0gu44y=wgr+8nrak!@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
